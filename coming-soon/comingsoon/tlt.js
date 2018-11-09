79aee6304d1ceb0bb084df572b0e3d23a96c9b9c
giabr <31702129+giabr@users.noreply.github.com>
2018-11-09 18:00:29 +0700

    coming soon folder

---
+++ coming-soon/comingsoon/tlt.js
@@ -0,0 +1,22 @@
+$('.tlt').textillate({
+	loop: true,
+	minDisplayTime: 2000,
+	initialDelay: 0,
+	autoStart: true,
+	in: {
+  	// set the effect name
+    effect: 'fadeInUp',
+    // set the delay factor applied to each consecutive character
+    delayScale: 1.5,
+    // set the delay between each character
+    delay: 450,
+    shuffle: true
+  },
+  // out animation settings.
+  out: {
+    effect: 'fadeOutDown',
+    delayScale: 1.5,
+    delay: 150,
+    shuffle: true,
+  }
+});
